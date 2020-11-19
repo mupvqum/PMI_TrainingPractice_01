5a7568b705b2236a4163a26d3eebe5b7c16087e8
mupvqum <maybe.ma.polit00@mail.ru>
2020-11-19 21:12:44 +0300

    Zadacha_2

---
+++ Zadacha_2/PMI_Task_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PMI_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("ВЫ можете написать тут свой текст, для выхода напишите exit"); //Обращение к пользователю
+
+            string message = (Console.ReadLine( )); //Инициализация переменной в которую копируются значения, введенну пользователем
+          
+
+            while (true) //Условия цикла всегда true, вечный цикл
+            {
+                if (message.CompareTo("exit") == 0) //Сравление значения переменной с нужным словом
+                {
+                    break; //Остановка цикла
+                }
+                message = Console.ReadLine(); //Ввод значения с клавиатуры
+            }
+        }
+    }
+}
