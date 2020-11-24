aa9f4c0011675d8aae51d7967fa3ecf3b51b2ac9
mupvqum <maybe.ma.polit00@mail.ru>
2020-11-24 20:44:10 +0300

    PMI_Task_05

---
+++ Zadacha_5/PMI_Task_05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace PMI_Task_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Create("Maze");
+        }
+    }
+}
